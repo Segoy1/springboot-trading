40fc935506ca065b25c512d496d1ea2607a57583
Segoy <segoy.thiel@protonmail.com>
2023-10-31 10:08:39 +0100

    resolved Datasource issues in multi Module Project

--- springboot-trading-web/src/main/java/de/segoy/springboottradingweb/SpringbootTradingApplication.java
+++ springboot-trading-web/src/main/java/de/segoy/springboottradingweb/SpringbootTradingApplication.java
@@ -2,8 +2,12 @@ package de.segoy.springboottradingweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "de.segoy.springboottradingdata.dao")
+@EntityScan(basePackages = "de.segoy.springboottradingdata.ds")
 public class SpringbootTradingApplication {
 
 	public static void main(String[] args) {
