83d40209387823942a1738c0ed7655a7a2ccdec5
Segoy <segoy.thiel@protonmail.com>
2023-11-02 11:05:39 +0100

    enabled h2-console

--- springboot-trading-web/src/main/java/de/segoy/springboottradingweb/security/WebSecurityConfig.java
+++ springboot-trading-web/src/main/java/de/segoy/springboottradingweb/security/WebSecurityConfig.java
@@ -1,14 +1,13 @@
 package de.segoy.springboottradingweb.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
@@ -18,7 +17,6 @@ import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 import javax.sql.DataSource;
@@ -30,24 +28,26 @@ import static de.segoy.springboottradingweb.security.SecurityRoles.*;
 public class WebSecurityConfig {
 
     @Bean
-   public SecurityFilterChain securityFilterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
         MvcRequestMatcher.Builder mvc = new MvcRequestMatcher.Builder(introspector).servletPath("/");
         http
                 .authorizeHttpRequests((requests) -> requests
-                .requestMatchers(mvc.pattern("/"),mvc.pattern("/home")).permitAll()
-                .requestMatchers(mvc.pattern("/employees")).hasRole(EMPLOYEES_PAG_VIEW)
-                .requestMatchers(mvc.pattern("/departments")).hasRole(DEPARTMENTS_PAG_VIEW)
-                .requestMatchers(mvc.pattern("/customers")).hasRole(CUSTOMERS_PAG_VIEW)
-                .anyRequest().authenticated())
-                .formLogin(form  -> form
+                        .requestMatchers(mvc.pattern("/"), mvc.pattern("/home")).permitAll()
+                        .requestMatchers(mvc.pattern("/employees")).hasRole(EMPLOYEES_PAG_VIEW)
+                        .requestMatchers(mvc.pattern("/departments")).hasRole(DEPARTMENTS_PAG_VIEW)
+                        .requestMatchers(mvc.pattern("/customers")).hasRole(CUSTOMERS_PAG_VIEW)
+                        .requestMatchers(mvc.servletPath("/h2-console").pattern("**")).permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(form -> form
                         .loginPage("/login")
                         .failureUrl("/login-error")
                         .permitAll())
                 .logout(form -> form
                         .logoutUrl("/logout")
                         .logoutSuccessUrl("/login")
-                        .permitAll());
-
+                        .permitAll())
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers(httpSecurityHeadersConfigurer -> httpSecurityHeadersConfigurer.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
         return http.build();
     }
 
