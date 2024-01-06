58c7d66ed1297d1b3b4d0e809777bf6eafa7fc1c
Segoy <segoy.thiel@protonmail.com>
2024-01-06 14:46:42 +0100

    Add cancel all open Orders

--- springboot-trading-frontend/src/app/header/orders/service/order-cancel.service.ts
+++ springboot-trading-frontend/src/app/header/orders/service/order-cancel.service.ts
@@ -5,16 +5,16 @@ import {HttpHeaderService} from "../../shared/http-header.service";
 
 @Injectable()
 export class OrderCancelService{
-  private orderId : number;
   private errorMessage: string;
-  private url: string = 'http://localhost:8080/order/cancel-order';
+  private singleCancelUrl: string = 'http://localhost:8080/order/cancel-order';
+  private allCancelUrl: string = 'http://localhost:8080/order/cancel-all-open-orders';
 
   constructor(private httpClient: HttpClient, private httpHeaderService: HttpHeaderService) {
   };
 
-  cancelOrder(orderId:number) {
+  cancelSingleOrder(orderId:number) {
 
-    this.httpClient.delete(this.url, this.httpHeaderService.getHttpOptions()).subscribe({
+    this.httpClient.delete(this.singleCancelUrl, this.httpHeaderService.getHttpOptions()).subscribe({
       next: () => {
         console.log('success deleting order')
       },
@@ -23,4 +23,16 @@ export class OrderCancelService{
       }
     })
   }
+  cancelAllOpenOrders() {
+
+    this.httpClient.delete(this.allCancelUrl, this.httpHeaderService.getHttpOptions()).subscribe({
+      next: () => {
+        console.log('success deleting order')
+      },
+      error: (error) => {
+        this.errorMessage = error;
+      }
+    })
+  }
+
 }
