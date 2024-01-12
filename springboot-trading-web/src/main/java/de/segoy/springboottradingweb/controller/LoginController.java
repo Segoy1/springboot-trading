23b91ef13660ea072f73b3c0b812e7a35a254cd9
Segoy <segoy.thiel@protonmail.com>
2024-01-12 08:51:23 +0100

    Login Controller Saves Login in Session

--- springboot-trading-web/src/main/java/de/segoy/springboottradingweb/controller/LoginController.java
+++ springboot-trading-web/src/main/java/de/segoy/springboottradingweb/controller/LoginController.java
@@ -1,9 +1,14 @@
 package de.segoy.springboottradingweb.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -18,15 +23,20 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<Void> login(@RequestBody LoginRequest loginRequest) {
+    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest, HttpServletRequest request) {
         Authentication authenticationRequest =
                 UsernamePasswordAuthenticationToken.unauthenticated(loginRequest.username(), loginRequest.password());
         Authentication authenticationResponse =
                 this.authenticationManager.authenticate(authenticationRequest);
-        return ResponseEntity.ok().build();
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        securityContext.setAuthentication(authenticationResponse);
+        HttpSession session = request.getSession(true);
+        session.setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY, securityContext);
+        return ResponseEntity.ok("User successfully Logged in!");
     }
 
-    public record LoginRequest(String username, String password){}
+    public record LoginRequest(String username, String password) {
+    }
 
     @GetMapping("/login-error")
     public String loginError(Model model) {
