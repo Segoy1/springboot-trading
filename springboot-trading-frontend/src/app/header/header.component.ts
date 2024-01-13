e7aab89288d6fc011bce7b22fd0730dd5078a74c
Segoy <segoy.thiel@protonmail.com>
2024-01-13 19:45:19 +0100

    Add Login Dependent visibility of Navigation Elements

--- springboot-trading-frontend/src/app/header/header.component.ts
+++ springboot-trading-frontend/src/app/header/header.component.ts
@@ -1,24 +1,30 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, EventEmitter, OnDestroy, OnInit, Output} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {HttpService} from "./http.service";
 import {Router} from "@angular/router";
+import {Subscription} from "rxjs";
+import {LoginService} from "./login/login.service";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
-  @Output() headerSelectEvent: EventEmitter<string> = new EventEmitter<string>();
+export class HeaderComponent implements OnInit, OnDestroy{
   collapsed: boolean = false;
   isConnected:any = true;
+  isLoggedIn = true;
+  private userSubscription: Subscription;
 
-  constructor(private httpService:HttpService, private router:Router) {
+  constructor(private httpService:HttpService, private loginService:LoginService) {
   }
-
-  onHeaderClick(category:string){
-    this.headerSelectEvent.emit(category)
-    this.router.navigate([category]);
+  ngOnInit() {
+    this.userSubscription = this.loginService.user.subscribe((user)=>{
+      this.isLoggedIn = !!user;
+    })
+  }
+  ngOnDestroy() {
+    this.userSubscription.unsubscribe();
   }
 
   onConnect(connect:boolean){
