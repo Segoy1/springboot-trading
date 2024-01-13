ed8d8e4a69c889cc22c291d28bd0e981fbba8639
Segoy <segoy.thiel@protonmail.com>
2024-01-13 19:48:05 +0100

    Add Logout

--- springboot-trading-frontend/src/app/app-routing.module.ts
+++ springboot-trading-frontend/src/app/app-routing.module.ts
@@ -25,7 +25,7 @@ const appRoutes: Routes = [
     ]},
   {path: 'portfolio', component: PortfolioComponent},
   {path: 'market-data', component: MarketDataComponent},
-  {path: 'login', component: LoginComponent},
+  {path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
--- springboot-trading-frontend/src/app/header/header.component.ts
+++ springboot-trading-frontend/src/app/header/header.component.ts
@@ -10,36 +10,45 @@ import {LoginService} from "./login/login.service";
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent implements OnInit, OnDestroy{
+export class HeaderComponent implements OnInit, OnDestroy {
   collapsed: boolean = false;
-  isConnected:any = true;
+  isConnected: any = true;
   isLoggedIn = true;
   private userSubscription: Subscription;
 
-  constructor(private httpService:HttpService, private loginService:LoginService) {
+  constructor(private httpService: HttpService, private loginService: LoginService) {
   }
+
   ngOnInit() {
-    this.userSubscription = this.loginService.user.subscribe((user)=>{
+    this.userSubscription = this.loginService.user.subscribe((user) => {
       this.isLoggedIn = !!user;
     })
   }
+
   ngOnDestroy() {
     this.userSubscription.unsubscribe();
   }
 
-  onConnect(connect:boolean){
-    if(connect){
-    this.httpService.getConnect().subscribe(
-      response => {
-        console.log(response)
-        this.isConnected = response; },
-      (error) => { console.log(error); });
-    }if(!connect){
-    this.httpService.getDisconnect().subscribe((data) => {
-      console.log(data);
-      this.isConnected=data;
-    })
+  onConnect(connect: boolean) {
+    if (connect) {
+      this.httpService.getConnect().subscribe(
+        response => {
+          console.log(response)
+          this.isConnected = response;
+        },
+        (error) => {
+          console.log(error);
+        });
     }
+    if (!connect) {
+      this.httpService.getDisconnect().subscribe((data) => {
+        console.log(data);
+        this.isConnected = data;
+      })
+    }
+  }
+  onLogout(){
+    this.loginService.logout();
   }
 
 }
