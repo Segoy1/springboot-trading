55a75e80f670bead66d25e14b17abed1deb67673
Segoy <segoy.thiel@protonmail.com>
2024-01-16 08:39:41 +0100

    Optimize Modules in Frontend: Add LoginModule

--- springboot-trading-frontend/src/app/app-routing.module.ts
+++ springboot-trading-frontend/src/app/app-routing.module.ts
@@ -1,20 +1,19 @@
 import {RouterModule, Routes} from "@angular/router";
 import {HeaderComponent} from "./header/header.component";
 import {NgModule} from "@angular/core";
-import {LoginComponent} from "./header/login/login.component";
 import {MarketDataComponent} from "./header/market-data/market-data.component";
 
 const appRoutes: Routes = [
   {path: 'home', component: HeaderComponent},
-  {path: 'orders', loadChildren: () => import('./header/orders/orders.module').then(m=>m.OrdersModule)},
-  {path: 'portfolio', loadChildren: ()=> import('./header/portfolio/portfolio.module').then(m => m.PortfolioModule)},
+  {path: 'orders', loadChildren: () => import('./header/orders/orders.module').then(m => m.OrdersModule)},
+  {path: 'portfolio', loadChildren: () => import('./header/portfolio/portfolio.module').then(m => m.PortfolioModule)},
   {path: 'market-data', component: MarketDataComponent},
-  {path: 'login', component: LoginComponent}
+  {path: 'login', loadChildren: () => import('./header/login/login.module').then(m => m.LoginModule)}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule{
+export class AppRoutingModule {
 }
--- springboot-trading-frontend/src/app/app.module.ts
+++ springboot-trading-frontend/src/app/app.module.ts
@@ -1,33 +1,17 @@
-import {ApplicationRef, NgModule} from '@angular/core';
+import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
-import {OrdersComponent} from './header/orders/orders.component';
-import {PortfolioComponent} from './header/portfolio/portfolio.component';
 import {MarketDataComponent} from './header/market-data/market-data.component';
 import {DropdownDirective} from "./header/shared/dropdown.directive";
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {HistoicalDataComponent} from './header/histoical-data/histoical-data.component';
-import {LoginComponent} from './header/login/login.component';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {RouterLink, RouterLinkActive} from "@angular/router";
-import {CommonModule} from "@angular/common";
-import {PositionItemComponent} from './header/portfolio/position-list/position-item/position-item.component';
-import {OpenOrderItemComponent} from './header/orders/open-order-item/open-order-item.component';
-import {OrderFormComponent} from './header/orders/order-form/order-form.component';
 import {AppRoutingModule} from "./app-routing.module";
-import { OrderStartComponent } from './header/orders/order-start/order-start.component';
-import { ComboLegsComponent } from './header/orders/order-form/combo-legs/combo-legs.component';
-import { StrategyBuilderComponent } from './header/orders/order-form/strategy-builder/strategy-builder.component';
-import { LoadingSpinnerComponent } from './header/shared/loading-spinner/loading-spinner.component';
+import {LoadingSpinnerComponent} from './header/shared/loading-spinner/loading-spinner.component';
 import {LoginInterceptorService} from "./header/login/login-interceptor.service";
-import {AccountComponent} from "./header/portfolio/account/account.component";
-import {NotAvailablePipe} from "./header/shared/not-available.pipe";
-import { PositionListComponent } from './header/portfolio/position-list/position-list.component';
-import { AccountPnlComponent } from './header/portfolio/account/account-pnl/account-pnl.component';
-import { AccountMarginComponent } from './header/portfolio/account/account-margin/account-margin.component';
 
 
 @NgModule({
@@ -37,18 +21,16 @@ import { AccountMarginComponent } from './header/portfolio/account/account-margi
     MarketDataComponent,
     DropdownDirective,
     HistoicalDataComponent,
-    LoginComponent,
     LoadingSpinnerComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule,
     RouterLink,
     RouterLinkActive,
     AppRoutingModule
   ],
-  providers: [ {provide: HTTP_INTERCEPTORS, useClass: LoginInterceptorService, multi:true }],
+  providers: [{provide: HTTP_INTERCEPTORS, useClass: LoginInterceptorService, multi: true}],
   bootstrap: [AppComponent]
 })
 export class AppModule {
