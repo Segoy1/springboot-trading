3601dcbb0252e7e5853c60b569157e6591fe5595
Segoy <segoy.thiel@protonmail.com>
2024-01-26 12:15:01 +0100

    Rudimentary Layout for Market Data, and removed generic Ticks to have less data to display

--- springboot-trading-frontend/src/app/market-data/option-market-data-item/option-market-data-item.component.ts
+++ springboot-trading-frontend/src/app/market-data/option-market-data-item/option-market-data-item.component.ts
@@ -2,18 +2,21 @@ import {Component, Input, OnInit} from '@angular/core';
 import {OptionMarketData} from "../../model/market-data/option-market-data.model";
 import {MarketDataOpenCloseService} from "../service/market-data-open-close.service";
 import {Contract} from "../../model/contract.model";
-import {NgForOf} from "@angular/common";
+import {DecimalPipe, NgForOf} from "@angular/common";
 import {RouterLinkActive} from "@angular/router";
 import {StandardTicker} from "../../model/market-data/standard-ticker.model";
 import {OptionTicker} from "../../model/market-data/option-ticker.model";
+import {NotAvailablePipe} from "../../shared/not-available.pipe";
 
 @Component({
   selector: 'app-option-market-data-item',
   standalone: true,
-    imports: [
-        NgForOf,
-        RouterLinkActive
-    ],
+  imports: [
+    NgForOf,
+    RouterLinkActive,
+    DecimalPipe,
+    NotAvailablePipe
+  ],
   templateUrl: './option-market-data-item.component.html',
   styleUrl: './option-market-data-item.component.css'
 })
