bed40d1d7b79e1c8a9cd31f8d9a19cdafa215070
Segoy <segoy.thiel@protonmail.com>
2024-02-11 18:30:27 +0100

    Add Staging environment and configuration to use proper Environments

---
+++ springboot-trading-frontend/src/environments/environment.staging.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  apiUrl: 'http://staging.segoy.de/',
+  accountPnLTopic: 'ibkr_account_pnl',
+  positionsTopic: 'ibkr_positions',
+  ordersTopic: 'ibkr_order',
+  optionMarketDataTopic: 'ibkr_option_market_data',
+  standardMarketDataTopic: 'ibkr_standard_market_data',
+  errorMessageTopic: 'ibkr_error_message',
+  singlePnLTopic: 'ibkr_single_pnl'
+};
