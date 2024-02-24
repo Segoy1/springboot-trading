0c16dd059cea643e2c884cf5a46d77b537ece552
Segoy <segoy.thiel@protonmail.com>
2024-02-24 14:03:09 +0100

    Change Profile in deployment to staging. And adjusted

--- springboot-trading-data/src/main/java/de/segoy/springboottradingdata/config/KafkaConstantsConfig.java
+++ springboot-trading-data/src/main/java/de/segoy/springboottradingdata/config/KafkaConstantsConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 public class KafkaConstantsConfig {
 
 
-    @Value("${kafka.bootstrap-servers}")
+    @Value("${spring.kafka.bootstrap-servers}")
     private String BOOTSTRAP_SERVERS;
 
     @Value("${kafka.consumer.group.id}")
