eaeeec9ee073e3fe84dc9c470488d18895303f99
Segoy <segoy.thiel@protonmail.com>
2024-04-04 19:15:37 +0200

    Add Test To NextValidOrderIdGenerator

---
+++ springboot-trading-data/src/test/java/de/segoy/springboottradingdata/service/NextValidOrderIdGeneratorTest.java
@@ -0,0 +1,63 @@
+package de.segoy.springboottradingdata.service;
+
+import de.segoy.springboottradingdata.config.PropertiesConfig;
+import de.segoy.springboottradingdata.model.data.entity.OrderData;
+import de.segoy.springboottradingdata.repository.OrderDataRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class NextValidOrderIdGeneratorTest {
+
+    @Mock
+    private PropertiesConfig propertiesConfig;
+    @Mock
+    private OrderDataRepository orderDataRepository;
+    @InjectMocks
+    private NextValidOrderIdGenerator nextValidOrderIdGenerator;
+
+    @Test
+    void testGenerateNextOrderIdFromOrder(){
+        OrderData orderData = OrderData.builder().id(20L).build();
+
+        when(orderDataRepository.findTopByOrderByIdDesc()).thenReturn(Optional.of(orderData));
+        when(propertiesConfig.getNextValidOrderId()).thenReturn(19L,21L);
+
+        Long result = nextValidOrderIdGenerator.generateAndSaveNextOrderId(18);
+
+        assertEquals(21L, result);
+        verify(propertiesConfig, times(1)).setNextValidOrderId(21L);
+    }
+    @Test
+    void testGenerateNextOrderIdFromProperties(){
+        OrderData orderData = OrderData.builder().id(18L).build();
+
+        when(orderDataRepository.findTopByOrderByIdDesc()).thenReturn(Optional.of(orderData));
+        when(propertiesConfig.getNextValidOrderId()).thenReturn(19L);
+
+        nextValidOrderIdGenerator.generateAndSaveNextOrderId(18);
+
+        verify(propertiesConfig, times(1)).setNextValidOrderId(19L);
+    }
+    @Test
+    void testGenerateNextOrderIdFromParsedId(){
+        OrderData orderData = OrderData.builder().id(20L).build();
+
+        when(orderDataRepository.findTopByOrderByIdDesc()).thenReturn(Optional.of(orderData));
+        when(propertiesConfig.getNextValidOrderId()).thenReturn(19L, 42L);
+
+        Long result = nextValidOrderIdGenerator.generateAndSaveNextOrderId(42);
+
+        assertEquals(42L, result);
+        verify(propertiesConfig, times(1)).setNextValidOrderId(42L);
+    }
+
+}
