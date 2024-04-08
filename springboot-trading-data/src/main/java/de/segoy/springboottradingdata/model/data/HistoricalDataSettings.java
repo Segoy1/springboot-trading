9959445319b8a880a8cc4608395cf41f3bf8e56c
Segoy <segoy.thiel@protonmail.com>
2024-04-08 07:40:54 +0200

    Add Tests for HistoricalDataApiCaller

--- springboot-trading-data/src/main/java/de/segoy/springboottradingdata/model/data/HistoricalDataSettings.java
+++ springboot-trading-data/src/main/java/de/segoy/springboottradingdata/model/data/HistoricalDataSettings.java
@@ -13,7 +13,6 @@ import java.util.List;
 /**
  * Helper Class to get Historical Data Parameter right
  * Documentaion: <a href="https://interactivebrokers.github.io/tws-api/historical_bars.html">...</a>
- * <p>
  * Author: Segoy
  */
 @Getter
@@ -25,19 +24,24 @@ public class HistoricalDataSettings extends IBKRDataType {
 
     private ContractData contractData;
 
+    //End Date of historical Data
     private Timestamp backfillEndTime;
 
+    //Duration of Historical DAta
     private String backfillDuration;
 
+    //Time Frame of each Bar
     private BarSizeSetting barSizeSetting;
 
+    //e.G. Trades, Ask, Bid, BID_ASK ...
     private WhatToShowType whatToShow;
 
+    //2 gives linux Timestamp 1 gives yyyyMMdd
+    private int dateFormatStyle;//default 1
     //default Values unlikely to be changed
 
 
     private boolean regularTradingHours; // default= true, for client: 1=true 0=false
-    private int dateFormatStyle;//default 1
     private boolean keepUpToDate;//default false;
     private List<TagValue> chartOptions = new ArrayList<>();
 
