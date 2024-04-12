d98a6fc659dab5c2ad82fbd1ec47ed5af2f61a58
Segoy <segoy.thiel@protonmail.com>
2024-04-12 11:59:12 +0200

    Add Tests for StopAndStartMarketDataService

---
+++ springboot-trading-ibkr/src/test/java/de/segoy/springboottradingibkr/client/service/marketdata/StopAndStartMarketDataServiceTest.java
@@ -0,0 +1,46 @@
+package de.segoy.springboottradingibkr.client.service.marketdata;
+
+import de.segoy.springboottradingdata.model.data.entity.ContractData;
+import de.segoy.springboottradingdata.repository.ContractDataRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class StopAndStartMarketDataServiceTest {
+
+    @Mock
+    private ContractDataRepository contractDataRepository;
+    @Mock
+    private StartMarketDataApiCaller startMarketDataApiCaller;
+    @Mock
+    private StopMarketDataService stopMarketDataService;
+    @InjectMocks
+    private StopAndStartMarketDataService stopAndStartMarketDataService;
+
+    @Test
+    void testReinitiateApiCall() {
+        ContractData contractData = ContractData.builder().build();
+
+        when(contractDataRepository.findById(100L)).thenReturn(Optional.of(contractData));
+        stopAndStartMarketDataService.reinitiateApiCall(100);
+
+        verify(stopMarketDataService, times(1)).stopMarketDataForContractId(100);
+        verify(startMarketDataApiCaller, times(1)).callApi(contractData);
+    }
+    @Test
+    void testReinitiateApiCallAbsentData() {
+
+        when(contractDataRepository.findById(100L)).thenReturn(Optional.empty());
+        stopAndStartMarketDataService.reinitiateApiCall(100);
+
+        verify(stopMarketDataService, times(1)).stopMarketDataForContractId(100);
+        verify(startMarketDataApiCaller, never()).callApi(any(ContractData.class));
+    }
+}
