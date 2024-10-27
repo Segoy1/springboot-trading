ac63de5e4f83f2c37392637754c230480c8e126f
Segoy <segoy.thiel@protonmail.com>
2024-10-27 16:27:44 +0100

    added Id to position data if autotrade

--- springboot-trading-ibkr/src/main/java/de/segoy/springboottradingibkr/client/responsehandler/StreamsAggregatedPositionHandler.java
+++ springboot-trading-ibkr/src/main/java/de/segoy/springboottradingibkr/client/responsehandler/StreamsAggregatedPositionHandler.java
@@ -3,8 +3,10 @@ package de.segoy.springboottradingibkr.client.responsehandler;
 import de.segoy.springboottradingdata.model.data.entity.ContractDbo;
 import de.segoy.springboottradingdata.model.data.entity.PositionDbo;
 import de.segoy.springboottradingdata.model.data.kafka.PositionData;
+import de.segoy.springboottradingdata.model.subtype.Symbol;
 import de.segoy.springboottradingdata.modelconverter.PositionDataToDbo;
 import de.segoy.springboottradingdata.modelsynchronize.PositionDataDatabaseSynchronizer;
+import de.segoy.springboottradingdata.optionstradingservice.LastTradeDateBuilder;
 import de.segoy.springboottradingibkr.client.service.contract.UniqueContractDataProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -13,15 +15,28 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class StreamsAggregatedPositionHandler {
 
-    private final UniqueContractDataProvider uniqueContractDataProvider;
-    private final PositionDataDatabaseSynchronizer positionDataDatabaseSynchronizer;
-    private final PositionDataToDbo positionDataToDbo;
+  private final UniqueContractDataProvider uniqueContractDataProvider;
+  private final PositionDataDatabaseSynchronizer positionDataDatabaseSynchronizer;
+  private final PositionDataToDbo positionDataToDbo;
+  private final LastTradeDateBuilder lastTradeDateBuilder;
 
-    public PositionData persistContractAndPositionData(PositionData positionData) {
-        PositionDbo positionDbo = positionDataToDbo.convert(positionData);
-        ContractDbo persistedContract =
-                uniqueContractDataProvider.getExistingContractDataOrCallApi(positionDbo.getContractDBO()).orElseThrow();
-        positionDbo.setContractDBO(persistedContract);
-        return positionDataDatabaseSynchronizer.updateInDbOrSave(positionDbo).toKafkaPositionData();
+  public PositionData persistContractAndPositionData(PositionData positionData) {
+    PositionDbo positionDbo = positionDataToDbo.convert(positionData);
+    ContractDbo persistedContract =
+        uniqueContractDataProvider
+            .getExistingContractDataOrCallApi(positionDbo.getContractDBO())
+            .orElseThrow();
+    positionDbo.setContractDBO(persistedContract);
+    return positionDataDatabaseSynchronizer.updateInDbOrSave(positionDbo).toKafkaPositionData();
+  }
+
+  /**
+   * if LastTradeDate is today and Symbol is SPX it is Auto Trade
+   */
+  private void setIdIfAutoTrade(ContractDbo contractDbo, PositionDbo positionDbo) {
+    if (contractDbo.getLastTradeDate().equals(lastTradeDateBuilder.getDateStringFromToday())
+        && contractDbo.getSymbol().equals(Symbol.SPX)) {
+      positionDbo.setId(lastTradeDateBuilder.getDateLongFromToday());
     }
+  }
 }
