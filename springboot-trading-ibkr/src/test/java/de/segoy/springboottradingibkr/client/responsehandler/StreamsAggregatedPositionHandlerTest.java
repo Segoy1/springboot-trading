ca2d217a5ae55e92c43403d64fd185ed99e806ef
Segoy <segoy.thiel@protonmail.com>
2024-11-15 10:03:59 +0100

    removed Compile Errors

--- springboot-trading-ibkr/src/test/java/de/segoy/springboottradingibkr/client/responsehandler/StreamsAggregatedPositionHandlerTest.java
+++ springboot-trading-ibkr/src/test/java/de/segoy/springboottradingibkr/client/responsehandler/StreamsAggregatedPositionHandlerTest.java
@@ -1,10 +1,16 @@
 package de.segoy.springboottradingibkr.client.responsehandler;
 
+import de.segoy.springboottradingdata.config.TradeRuleSettingsConfig;
 import de.segoy.springboottradingdata.model.data.entity.ContractDbo;
 import de.segoy.springboottradingdata.model.data.entity.PositionDbo;
 import de.segoy.springboottradingdata.model.data.kafka.PositionData;
+import de.segoy.springboottradingdata.model.subtype.Strategy;
+import de.segoy.springboottradingdata.model.subtype.Symbol;
 import de.segoy.springboottradingdata.modelconverter.PositionDataToDbo;
 import de.segoy.springboottradingdata.modelsynchronize.PositionDataDatabaseSynchronizer;
+import de.segoy.springboottradingdata.optionstradingservice.AutotradeDbAndTickerIdEncoder;
+import de.segoy.springboottradingdata.optionstradingservice.LastTradeDateBuilder;
+import de.segoy.springboottradingdata.service.StrategyNameService;
 import de.segoy.springboottradingibkr.client.service.contract.UniqueContractDataProvider;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -22,50 +28,82 @@ import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 class StreamsAggregatedPositionHandlerTest {
 
-    @Mock
-    private UniqueContractDataProvider uniqueContractDataProvider;
-    @Mock
-    private PositionDataDatabaseSynchronizer positionDataDatabaseSynchronizer;
-    @Mock
-    private PositionDataToDbo positionDataToDbo;
-    @InjectMocks
-    private StreamsAggregatedPositionHandler streamsAggregatedPositionHandler;
+  @Mock private UniqueContractDataProvider uniqueContractDataProvider;
+  @Mock private PositionDataDatabaseSynchronizer positionDataDatabaseSynchronizer;
+  @Mock private PositionDataToDbo positionDataToDbo;
+  @Mock private LastTradeDateBuilder lastTradeDateBuilder;
+  @Mock private TradeRuleSettingsConfig tradeRuleSettingsConfig;
+  @Mock private StrategyNameService strategyNameService;
+  @Mock private AutotradeDbAndTickerIdEncoder autotradeDbAndTickerIdEncoder;
+  @InjectMocks private StreamsAggregatedPositionHandler streamsAggregatedPositionHandler;
 
-    @Test
-    void testError(){
-        ContractDbo contractDboOld = ContractDbo.builder().contractId(1).build();
-        PositionDbo positionDBO = PositionDbo.builder().contractDBO(contractDboOld).build();
-        ContractDbo contractDboNew = ContractDbo.builder().contractId(2).build();
+  @Test
+  void testError() {
+    ContractDbo contractDboOld = ContractDbo.builder().contractId(1).build();
+    PositionDbo positionDBO = PositionDbo.builder().contractDBO(contractDboOld).build();
+    ContractDbo contractDboNew = ContractDbo.builder().contractId(2).build();
 
-        when(uniqueContractDataProvider.getExistingContractDataOrCallApi(contractDboOld))
-                .thenReturn(Optional.empty());
-        when(positionDataToDbo.convert(any())).thenReturn(positionDBO);
+    when(uniqueContractDataProvider.getExistingContractDataOrCallApi(contractDboOld))
+        .thenReturn(Optional.empty());
+    when(positionDataToDbo.convert(any())).thenReturn(positionDBO);
 
+    Exception e =
+        assertThrows(
+            RuntimeException.class,
+            () -> {
+              streamsAggregatedPositionHandler.persistContractAndPositionData(
+                  positionDBO.toKafkaPositionData());
+            });
 
+    assertThat(e.getMessage()).isEqualTo("No value present");
+  }
 
-        Exception e =
-                assertThrows(RuntimeException.class, ()->{
-                        streamsAggregatedPositionHandler.persistContractAndPositionData(positionDBO.toKafkaPositionData());
-                        });
+  @Test
+  void testPersistWithId() {
+    ContractDbo contractDboOld =
+        ContractDbo.builder().lastTradeDate("20241115").symbol(Symbol.SPX).contractId(1).build();
+    PositionDbo positionDBO = PositionDbo.builder().contractDBO(contractDboOld).build();
+    ContractDbo contractDboNew =
+        ContractDbo.builder().lastTradeDate("20241115").symbol(Symbol.SPX).contractId(2).build();
 
-        assertThat(e.getMessage()).isEqualTo("No value present");
+    when(uniqueContractDataProvider.getExistingContractDataOrCallApi(contractDboOld))
+        .thenReturn(Optional.of(contractDboNew));
+    when(tradeRuleSettingsConfig.getTradeSymbol()).thenReturn(Symbol.SPX);
+    when(positionDataDatabaseSynchronizer.updateInDbOrSave(positionDBO))
+        .thenReturn(Optional.of(positionDBO));
+    when(positionDataToDbo.convert(any())).thenReturn(positionDBO);
+    when(lastTradeDateBuilder.getDateStringFromToday()).thenReturn("20241115");
+    when(strategyNameService.resolveStrategyFromComboLegs(any())).thenReturn(Strategy.IRON_CONDOR);
+    when(autotradeDbAndTickerIdEncoder.generateLongForTodayBySymbolAndStrategy(
+            Symbol.SPX, Strategy.IRON_CONDOR))
+        .thenReturn(31241115L);
 
-    }
-    @Test
-    void testPersist(){
-        ContractDbo contractDboOld = ContractDbo.builder().contractId(1).build();
-        PositionDbo positionDBO = PositionDbo.builder().contractDBO(contractDboOld).build();
-        ContractDbo contractDboNew = ContractDbo.builder().contractId(2).build();
-
-        when(uniqueContractDataProvider.getExistingContractDataOrCallApi(contractDboOld))
-                .thenReturn(Optional.of(contractDboNew));
-        when(positionDataDatabaseSynchronizer.updateInDbOrSave(positionDBO)).thenReturn(positionDBO);
-        when(positionDataToDbo.convert(any())).thenReturn(positionDBO);
-    PositionData result =
+    Optional<PositionData> result =
         streamsAggregatedPositionHandler.persistContractAndPositionData(
             positionDBO.toKafkaPositionData());
 
-        assertThat(result.getContractData().getContractId()).isEqualTo(2);
-    }
+    assertThat(result.get().getContractData().getContractId()).isEqualTo(2);
+  }
 
+  @Test
+  void testPersistWithOutId() {
+    ContractDbo contractDboOld =
+        ContractDbo.builder().lastTradeDate("20241116").symbol(Symbol.SPX).contractId(1).build();
+    PositionDbo positionDBO = PositionDbo.builder().contractDBO(contractDboOld).build();
+    ContractDbo contractDboNew =
+        ContractDbo.builder().lastTradeDate("20241116").symbol(Symbol.SPX).contractId(2).build();
+
+    when(uniqueContractDataProvider.getExistingContractDataOrCallApi(contractDboOld))
+        .thenReturn(Optional.of(contractDboNew));
+    when(positionDataDatabaseSynchronizer.updateInDbOrSave(positionDBO))
+        .thenReturn(Optional.of(positionDBO));
+    when(positionDataToDbo.convert(any())).thenReturn(positionDBO);
+    when(lastTradeDateBuilder.getDateStringFromToday()).thenReturn("20241115");
+
+    Optional<PositionData> result =
+        streamsAggregatedPositionHandler.persistContractAndPositionData(
+            positionDBO.toKafkaPositionData());
+
+    assertThat(result.get().getContractData().getContractId()).isEqualTo(2);
+  }
 }
--- springboot-trading-web/src/main/java/de/segoy/springboottradingweb/websocket/ApiResponseKafkaHandler.java
+++ springboot-trading-web/src/main/java/de/segoy/springboottradingweb/websocket/ApiResponseKafkaHandler.java
@@ -18,66 +18,84 @@ import org.springframework.transaction.annotation.Transactional;
 @Slf4j
 public class ApiResponseKafkaHandler {
 
-    private final KafkaConstantsConfig kafkaConstantsConfig;
-    private final SimpMessagingTemplate messagingTemplate;
-    private final ErrorCodeMapper errorCodeMapper;
-    private final StreamsAggregatedPositionHandler streamsAggregatedPositionHandler;
+  private final KafkaConstantsConfig kafkaConstantsConfig;
+  private final SimpMessagingTemplate messagingTemplate;
+  private final ErrorCodeMapper errorCodeMapper;
+  private final StreamsAggregatedPositionHandler streamsAggregatedPositionHandler;
 
-    @KafkaListener(groupId = "${kafka.consumer.group.id}", topics = "${kafka.names.topic.errorMessage}")
-    public void consumeOptionMarketDataMessage(ErrorMessage errorMessage) {
-        String topic = kafkaConstantsConfig.getERROR_MESSAGE_TOPIC();
-        errorCodeMapper.mapError(errorMessage).ifPresent(handledErrorMessage -> {
-            messagingTemplate.convertAndSend("/topic/" + topic, handledErrorMessage);
-        });
-    }
-    @KafkaListener(groupId = "${kafka.consumer.group.id}", topics = "${kafka.names.topic.singlePnL}")
-    public void consumeMessage(ProfitAndLossData message) {
-        log.info("PNL Message received: " + message.getId());
-        messagingTemplate.convertAndSend("/topic/" + kafkaConstantsConfig.getSINGLE_PNL_TOPIC(), message);
-    }
+  @KafkaListener(
+      groupId = "${kafka.consumer.group.id}",
+      topics = "${kafka.names.topic.errorMessage}")
+  public void consumeOptionMarketDataMessage(ErrorMessage errorMessage) {
+    String topic = kafkaConstantsConfig.getERROR_MESSAGE_TOPIC();
+    errorCodeMapper
+        .mapError(errorMessage)
+        .ifPresent(
+            handledErrorMessage -> {
+              messagingTemplate.convertAndSend("/topic/" + topic, handledErrorMessage);
+            });
+  }
 
-    @KafkaListener(groupId = "${kafka.consumer.group.id}", topics = "${kafka.names.topic.accountPnL}")
-    public void consumePnLMessage(ProfitAndLossData message) {
-        String topic = kafkaConstantsConfig.getACCOUNT_PNL_TOPIC();
-        log.info("Message received: " + topic + "-" + message.getId());
-        messagingTemplate.convertAndSend("/topic/" + topic, message);
-    }
+  @KafkaListener(groupId = "${kafka.consumer.group.id}", topics = "${kafka.names.topic.singlePnL}")
+  public void consumeMessage(ProfitAndLossData message) {
+    log.info("PNL Message received: " + message.getId());
+    messagingTemplate.convertAndSend(
+        "/topic/" + kafkaConstantsConfig.getSINGLE_PNL_TOPIC(), message);
+  }
 
-    @KafkaListener(groupId = "${kafka.consumer.group.id}", topics = "${kafka.names.topic.accountSummary}")
-    public void consumeMarketDataMessage(AccountSummaryData message) {
-        String summaryTopic = kafkaConstantsConfig.getACCOUNT_SUMMARY_TOPIC();
-        log.info("Message received: " + summaryTopic + "-" + message.getTag());
-        messagingTemplate.convertAndSend("/topic/" + summaryTopic, message);
-    }
+  @KafkaListener(groupId = "${kafka.consumer.group.id}", topics = "${kafka.names.topic.accountPnL}")
+  public void consumePnLMessage(ProfitAndLossData message) {
+    String topic = kafkaConstantsConfig.getACCOUNT_PNL_TOPIC();
+    log.info("Message received: " + topic + "-" + message.getId());
+    messagingTemplate.convertAndSend("/topic/" + topic, message);
+  }
 
-    @KafkaListener(groupId = "${kafka.consumer.group.id}", topics = "${kafka.names.topic.optionMarketData}")
-    public void consumeOptionMarketDataMessage(OptionMarketData message) {
-        String topic = kafkaConstantsConfig.getOPTION_MARKET_DATA_TOPIC();
-        messagingTemplate.convertAndSend("/topic/" + topic, message);
-    }
+  @KafkaListener(
+      groupId = "${kafka.consumer.group.id}",
+      topics = "${kafka.names.topic.accountSummary}")
+  public void consumeMarketDataMessage(AccountSummaryData message) {
+    String summaryTopic = kafkaConstantsConfig.getACCOUNT_SUMMARY_TOPIC();
+    log.info("Message received: " + summaryTopic + "-" + message.getTag());
+    messagingTemplate.convertAndSend("/topic/" + summaryTopic, message);
+  }
 
-    @KafkaListener(groupId = "${kafka.consumer.group.id}", topics = "${kafka.names.topic.standardMarketData}")
-    public void consumeMarketDataMessage(StandardMarketData message) {
-        String standardMarketTopic = kafkaConstantsConfig.getSTANDARD_MARKET_DATA_TOPIC();
-        messagingTemplate.convertAndSend("/topic/" + standardMarketTopic, message);
-    }
+  @KafkaListener(
+      groupId = "${kafka.consumer.group.id}",
+      topics = "${kafka.names.topic.optionMarketData}")
+  public void consumeOptionMarketDataMessage(OptionMarketData message) {
+    String topic = kafkaConstantsConfig.getOPTION_MARKET_DATA_TOPIC();
+    messagingTemplate.convertAndSend("/topic/" + topic, message);
+  }
 
-    @KafkaListener(groupId = "${kafka.consumer.group.id}", topics = "${kafka.names.topic.orderData}")
-    public void consumeMessage(OrderData message) {
-        log.info("Order received: " + message.getId());
-        messagingTemplate.convertAndSend("/topic/" + kafkaConstantsConfig.getORDER_TOPIC(), message);
-    }
+  @KafkaListener(
+      groupId = "${kafka.consumer.group.id}",
+      topics = "${kafka.names.topic.standardMarketData}")
+  public void consumeMarketDataMessage(StandardMarketData message) {
+    String standardMarketTopic = kafkaConstantsConfig.getSTANDARD_MARKET_DATA_TOPIC();
+    messagingTemplate.convertAndSend("/topic/" + standardMarketTopic, message);
+  }
 
-    @KafkaListener(groupId = "${kafka.consumer.group.id}", topics = "${kafka.names.topic.positions}")
-    @Transactional
-    public void consumeMessage(PositionData message) {
-        if (message.getContractData().getSecurityType().equals(Types.SecType.BAG)) {
-            log.info("Streamed Message received: " + message);
-            PositionData updatedPosition = streamsAggregatedPositionHandler.persistContractAndPositionData(message);
-            messagingTemplate.convertAndSend("/topic/" + kafkaConstantsConfig.getPOSITION_TOPIC(), updatedPosition);
-        }else{
-        log.info("Message received: " + message);
-        messagingTemplate.convertAndSend("/topic/" + kafkaConstantsConfig.getPOSITION_TOPIC(), message);
-        }
+  @KafkaListener(groupId = "${kafka.consumer.group.id}", topics = "${kafka.names.topic.orderData}")
+  public void consumeMessage(OrderData message) {
+    log.info("Order received: " + message.getId());
+    messagingTemplate.convertAndSend("/topic/" + kafkaConstantsConfig.getORDER_TOPIC(), message);
+  }
+
+  @KafkaListener(groupId = "${kafka.consumer.group.id}", topics = "${kafka.names.topic.positions}")
+  @Transactional
+  public void consumeMessage(PositionData message) {
+    if (message.getContractData().getSecurityType().equals(Types.SecType.BAG)) {
+      log.info("Streamed Message received: " + message);
+      streamsAggregatedPositionHandler
+          .persistContractAndPositionData(message)
+          .ifPresent(
+              (updatedPosition) ->
+                  messagingTemplate.convertAndSend(
+                      "/topic/" + kafkaConstantsConfig.getPOSITION_TOPIC(), updatedPosition));
+    } else {
+      log.info("Message received: " + message);
+      messagingTemplate.convertAndSend(
+          "/topic/" + kafkaConstantsConfig.getPOSITION_TOPIC(), message);
     }
+  }
 }
