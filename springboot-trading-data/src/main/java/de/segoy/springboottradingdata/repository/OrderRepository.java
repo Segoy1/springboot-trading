89244709d2d887d89b214774ef5d73b0e46da00c
Segoy <segoy.thiel@protonmail.com>
2024-12-07 20:08:35 +0100

    added method findyByContractDbo

--- springboot-trading-data/src/main/java/de/segoy/springboottradingdata/repository/OrderRepository.java
+++ springboot-trading-data/src/main/java/de/segoy/springboottradingdata/repository/OrderRepository.java
@@ -1,10 +1,13 @@
 package de.segoy.springboottradingdata.repository;
 
+import de.segoy.springboottradingdata.model.data.entity.ContractDbo;
 import de.segoy.springboottradingdata.model.data.entity.OrderDbo;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface OrderRepository extends IBKRDataTypeRepository<OrderDbo> {
 
     Optional<OrderDbo> findTopByOrderByIdDesc();
+    List<OrderDbo> findByContractDBO(ContractDbo contractDbo);
 }
